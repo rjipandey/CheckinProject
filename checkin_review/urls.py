58a1b35303d97226dec745f14255dc9b8e229f1a
rjipandey <41576326+rjipandey@users.noreply.github.com>
2018-09-23 22:37:51 +0530

    Update urls.py
    2nd commit

--- checkin_review/urls.py
+++ checkin_review/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url('', views.home, name='home'),
     url(r'^login_user$', views.login_user, name='login_user'),
     url(r'^logout_user$', views.logout_user, name='logout_user'),
     url(r'^register_user$', views.register_user, name='register_user'),
